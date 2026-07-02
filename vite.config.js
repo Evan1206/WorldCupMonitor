047d0323d45f7b8c2bb3feea63409964a4fea706
Evanbobo <gogogo0118@gmail.com>
2026-07-02 19:59:13 +0800

    Bust cached globe assets

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash]-worldcup[extname]',
+        chunkFileNames: 'assets/[name]-[hash]-worldcup.js',
+        entryFileNames: 'assets/[name]-[hash]-worldcup.js',
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
